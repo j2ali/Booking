1e6fea05b9c31a848599e65499b1bb39e26885c6
safdarzeeshan <safdarzeeshan@gmail.com>
2014-08-16 18:39:15 -0400

    action added

--- BookingSite/reservations/urls.py
+++ BookingSite/reservations/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from reservations import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.calendar, name='calendar'),
+    url(r'^test/$', views.test, name='test')
 
 ]
--- BookingSite/reservations/views.py
+++ BookingSite/reservations/views.py
@@ -3,16 +3,32 @@ from django.shortcuts import render_to_response
 from django.template import Template, Context
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.utils import simplejson as json
 
 
 # Create your views here.
-def index(request):
+def calendar(request):
 
     t = get_template('calendar.html')
-    c = Context({'message': 'This is a Calender'})
+    c = Context({'message': 'This is a Calendar'})
 
     html = t.render(c)
 
     return HttpResponse(html)
+
+def test(request):
+
+    t = get_template('test.html')
+
+    year = request.GET['year']
+    month = request.GET['month']
+    day = request.GET['day']
+
+    c = Context({'message': year+day+month})
+
+    html = t.render(Context(c))
+    return HttpResponse(html)
+
+
 
 # Create your views here.
