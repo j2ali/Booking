90eece348bc57d2367be208c937a852558d1a17a
Jared Ali <jared@wattpad.com>
2014-08-17 01:41:55 -0400

    Remove attendance

--- reservations/helper.py
+++ reservations/helper.py
@@ -5,7 +5,6 @@ def get_available_time_slots(hours, appointments, duration=timedelta(hours=1)):
     slots = sorted([(hours[0], hours[0])] + appointments + [(hours[1], hours[1])])
     available_time_slots = []
     for start, end in ((slots[i][1], slots[i + 1][0]) for i in range(len(slots) - 1)):
-        assert start <= end, "Cannot attend all appointments"
         while start + duration <= end:
             available_time_slots.append("{:%H:%M} - {:%H:%M}".format(start, start + duration))
             start += duration
