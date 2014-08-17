0e905b934cebf608d6adaf3e58b16f193c1303ad
Jared Ali <jared@wattpad.com>
2014-08-16 20:10:09 -0400

    Added finding timeslot

--- BookingSite/reservations/views.py
+++ BookingSite/reservations/views.py
@@ -4,6 +4,7 @@ from models import *
 from django.template import Template, Context
 from django.http import HttpResponse
 from django.template.loader import get_template
+import datetime
 from django.utils import simplejson as json
 
 
@@ -18,15 +19,21 @@ def calendar(request):
 def test(request):
     t = get_template('test.html')
 
-    year = request.GET['year']
-    month = request.GET['month']
-    day = request.GET['day']
+    year = int(request.GET['year'])
+    month = int(request.GET['month'])
+    day = int(request.GET['day'])
 
-    c = Context({'message': year+day+month})
+    start_date = datetime.date(year, month, day)
+    end_date = start_date + datetime.timedelta(days=1)
+
+    if Reservation.objects.filter(time_slot__year=year, time_slot__month=month, time_slot__day=day).count() == 0:
+        test = 'Nothing in DB'
+    test = 'Oh snap!'
+
+    c = Context({'message': test})
 
     html = t.render(Context(c))
     return HttpResponse(html)
-
 
 
 # Create your views here.
