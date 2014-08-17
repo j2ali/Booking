b1d37a9cc2c1fed54deb1c8f894018d8d13f87b1
Jared Ali <jared@wattpad.com>
2014-08-16 20:37:06 -0400

    Format

--- reservations/models.py
+++ reservations/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
+
 class Reservation(models.Model):
 
     time_slot = models.DateTimeField(null=False, blank=False)
     patient_info = models.CharField(max_length=500, blank=False)
-
-
-
-# Create your models here.
