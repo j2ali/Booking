ac05927aaf967d98be0c21f38f31a1c4bf02cc1e
Jared Ali <jared@wattpad.com>
2014-08-17 21:05:28 -0400

    Adding admin site settings

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from reservations.models import Reservation
 
 # Register your models here.
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ['patient_info', 'time_slot']
+    list_display = ('patient_info', 'time_slot')
+
+admin.site.register(Reservation, ReservationAdmin)
--- reservations/models.py
+++ reservations/models.py
@@ -5,3 +5,6 @@ class Reservation(models.Model):
 
     time_slot = models.DateTimeField(null=False, blank=False)
     patient_info = models.CharField(max_length=500, blank=False)
+
+    def __str__(self):
+        return self.patient_info
