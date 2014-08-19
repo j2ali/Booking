ed116a1f5a067623f9d7812aae7921032beaa537
Azhar Khan <azmanian.devil@gmail.com>
2014-08-18 21:52:08 -0400

    home url goes to calendar view

--- BookingSite/urls.py
+++ BookingSite/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from reservations import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'BookingSite1.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.calendar),
     url(r'^reservations/', include('reservations.urls')),
 )
